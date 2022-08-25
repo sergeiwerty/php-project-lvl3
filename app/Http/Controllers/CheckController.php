252c83153c14c4bc4eab483397ef1ef3a4bef9db
sergeiwerty <dshl.srp@gmail.com>
2022-08-25 15:28:35 +0300

    add try catch block to url check controller

--- app/Http/Controllers/CheckController.php
+++ app/Http/Controllers/CheckController.php
@@ -13,49 +13,53 @@ class CheckController extends Controller
 {
     public function store(Request $request, $id)
     {
-        $urlName = DB::table('urls')
-            ->select('name')
-            ->where('id', '=', $id)
-            ->get()
-            ->first();
+        try {
+            $urlName = DB::table('urls')
+                ->select('name')
+                ->where('id', '=', $id)
+                ->get()
+                ->first();
 
-        dump($urlName);
+            $status = Http::get($urlName->name)->status();
 
-        $status = Http::get($urlName->name)->status();
+            $document = new Document($urlName->name, true);
 
-        $document = new Document($urlName->name, true);
+            if ($document->has('h1')) {
+                $h1 = $document->first('h1');
+            } else {
+                $h1 = null;
+            }
 
-        if ($document->has('h1')) {
-            $h1 = $document->first('h1');
-        } else {
-            $h1 = null;
+            if ($document->has('title')) {
+                $title = $document->first('title');
+            } else {
+                $title = null;
+            }
+
+            if ($document->has('meta[name="description"][content]')) {
+                $metaContent = $document->first('meta[name="description"][content]');
+            } else {
+                $metaContent = null;
+            }
+
+            $h1Text = optional($h1)->text();
+            $titleText = optional($title)->text();
+            $metaContentText = optional($metaContent)->getAttribute('content');
+
+            DB::table('url_checks')
+                ->insert([
+                    'url_id' =>  $id,
+                    'status_code' => $status,
+                    'h1' => $h1Text,
+                    'title' => $titleText,
+                    'description' => $metaContentText,
+                    'created_at' => Carbon::now('+03:00'),
+                ]);
+        } catch (\Exception $exception) {
+            flash($exception->getMessage())->error();
+            return back();
         }
 
-        if ($document->has('title')) {
-            $title = $document->first('title');
-        } else {
-            $title = null;
-        }
-
-        if ($document->has('meta[name="description"][content]')) {
-            $metaContent = $document->first('meta[name="description"][content]');
-        } else {
-            $metaContent = null;
-        }
-
-        $h1Text = optional($h1)->text();
-        $titleText = optional($title)->text();
-        $metaContentText = optional($metaContent)->getAttribute('content');
-
-        DB::table('url_checks')
-            ->insert([
-                'url_id' =>  $id,
-                'status_code' => $status,
-                'h1' => $h1Text,
-                'title' => $titleText,
-                'description' => $metaContentText,
-                'created_at' => Carbon::now('+03:00'),
-            ]);
 
 //        $checkData = DB::table('url_checks')
 //            ->select('*')
