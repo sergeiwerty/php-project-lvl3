763f542ad42da27befde50b13f22509c0d138de3
sergeiwerty <dshl.srp@gmail.com>
2022-09-20 23:33:26 +0300

    edit vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,9 @@ import path from 'path'
 
 export default defineConfig({
     plugins: [
-        laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
+        laravel([
+            'resources/js/app.js',
+        ]),
     ],
     server: {
         host: '0.0.0.0',
